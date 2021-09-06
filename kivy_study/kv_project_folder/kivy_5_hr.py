3a0aabe5bac951b94e03f6b6c74388ecbc57d8bd
arsakif <ars.akif@gmail.com>
2021-09-06 15:41:20 -0400

    first

--- kivy_study/kv_project_folder/kivy_5_hr.py
+++ kivy_study/kv_project_folder/kivy_5_hr.py
@@ -1,26 +1,80 @@
 from kivy.app import App
+from kivy.metrics import dp
+from kivy.properties import StringProperty
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 
 
+class WidgetExample(GridLayout):
+    c = 0
+    my_text = StringProperty('Rezzan-' + str(c))
+    def on_button_click(self):
+        self.c += 1
+        self.my_text = 'Rezzan-' + str(self.c)
+
+
 class BoxLayoutExample(BoxLayout):
-    def __init__(self, **kwargs):
-        super(BoxLayoutExample, self).__init__(**kwargs)
+    pass
+    """ def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.orientation = 'vertical'
-        b1 = Button(text='Button-1')
-        b2 = Button(text='Button-2')
-        b3 = Button(text='Button-3')
+        b1 = Button(text="Akif")
+        b2 = Button(text="Rezzan")
+        b3 = Button(text="C")
+
         self.add_widget(b1)
         self.add_widget(b2)
-        self.add_widget(b3)
+        self.add_widget(b3) """
+
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+    """ def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
+        b1 = Button(text="Akif")
+        b2 = Button(text="Rezzan")
+        b3 = Button(text="C")
+
+        self.add_widget(b1)
+        self.add_widget(b2)
+        self.add_widget(b3) """
+
+
+class GridLayoutExample(GridLayout):
+    pass
+    """ def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
+        b1 = Button(text="Akif")
+        b2 = Button(text="Rezzan")
+        b3 = Button(text="C")
+
+        self.add_widget(b1)
+        self.add_widget(b2)
+        self.add_widget(b3) """
+
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        spacing = (dp(20), dp(20))
+        for i in range(1, 100):
+            sz = dp(100)
+            b = Button(text=f'Rez-{str(i)}', size_hint=(None, None),
+                       size=(sz, sz))
+            self.add_widget(b)
 
 
 class TheLabApp(App):
     pass
 
 
-class MainWidget(Widget):
+class MainWidgetExample(Widget):
     pass
 
 
